7ff434bc4e087b059a97ca4aa12366aa9a047643
sashshaikh12 <sashshaikh12@gmail.com>
2025-05-25 13:51:03 +0530

    installed shadcn and its charts

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -1,7 +1,6 @@
-import {React, useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { FaStar, FaRegStar } from "react-icons/fa";
-import { set } from "mongoose";
 
 function Challenges() {
 
